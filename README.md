538cf0a1ef836e4c73332d42a7a77f423e04f145
Ross Ewing <mail@smc.digital>
2017-02-01 19:02:25 +0000

    Update to underscore and Readme

--- README.md
+++ README.md
@@ -1,12 +1,59 @@
-# ec2-backup
+# EC2-Backup
 
-Public Versioned ec2-backup node script to run multiple snapshots
+Public Version ec2-backup node script to run multiple snapshots
 of Amazon Web Service EC2 Volumes and then delete them after a required period.
 
-Clone Repo where you would like to run the script from. 
+User only needs to update the config.js file with their relevant information and the package will work.
 
-$ Git clone https://github.com/ross-ewing/EC2-Backup.git
+##How to install:
 
-!! npm install
+1) Clone Repo to the location you wish to install by running the below script into your terminal.
 
-Edit config.js to your own details 
+```
+$ git clone https://github.com/ross-ewing/EC2-Backup.git
+```
+2) Run the below node package installer command.
+```
+npm install
+```
+3) Edit the config.js to your own details from aws. ( if your not sure how to obtain these details read AWS config.js details below)
+
+##AWS config.js details
+
+1) the SMTP auth: { user: 'USERNAME', pass: 'PASSWORD',
+
+* First login to your amazon dashboard, we are assuming you have an account considering you're trying to backup an AWS volume.
+* Navigate to the AWS SES tool and click "SMTP Settings" in the left panel
+* Click "Create My SMTP Credentials"
+* Type the given values into the config.js after you go through the given setup of an IAM user.
+
+2) amazon: { secretKey: 'SECRET KEY', accessKey: 'ACCESS',
+
+* Navigate to IAM Users and select create new user
+* When given the security details take note
+* Add the EC2 permissions so that the user can access the data.
+* add the details to the amazon secret key and access key in the config.js file
+
+3) You can now get your volumeID from the EC2 section of AWS and enter them into the VolumeId Array in the config.js.
+
+(Note: that the Volume Names are in order of the volume ID's, these names can be anything)
+
+## Add Volume 
+To add more volumes simply add it to the end of the Volume Id Array in config.js
+
+(Note: Keep in mind the "backupint" in config.js, this is the time in-between each volumes backup. If your volumes are quite large, increase this to allow AWS to finish backing up the previous volume. )
+
+
+## Contributing
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request
+
+## Credits
+* AWS 
+* [Ross Ewing](https://twitter.com/ross_ewing?lang=en)
+* [Chris Sewell](https://twitter.com/chris_sewell?lang=en)
+
+
--- backup.js
+++ backup.js
@@ -1,30 +1,29 @@
 var AWS = require('aws-sdk');
 var config = require('./config.js').config;
 var Mailer = require('./modules/mailer.js');
+var _ = require('underscore');
 
 AWS.config.accessKeyId = config.amazon.accessKey;
 AWS.config.secretAccessKey = config.amazon.secretKey;
 AWS.config.region = config.amazon.region;
 AWS.config.email = config.amazon.email;
 
-var awsvolumeid = function (volumeId) {
+// For each Volume place Id into array
+
+var awsvolumeid = _.each(volumeId, function (volumeId) {
     values = [];
-    for (var i = 0; i < volumeId.length; i++) {
-        values.push(volumeId[i]);
-    }
-    console.log(values);
+    values.push(volumeId);
     return values;
-};
+});
 
-var awsvolumeName = function (volumeId) {
+var awsvolumeName = _.each(volumeName, function () {
     values = [];
-    for (var i = 0; i < volumeId.length; i++) {
-        values.push(volumeName[i]);
-    }
-    return values.join('');
-};
+    values.push(volumeName);
+    return values;
+});
 
-var mailer = new Mailer(config.smtp);
+//var mailer = new Mailer(config.smtp);
+
 var emailAddress = config.smtp.email;
 var sendEmailAddress = config.smtp.sendEmail;
 var timestampNow = new Date().getTime();
@@ -34,82 +33,81 @@ var params = {
     Filters: [
         {
             Name: 'volume-id',
-            Values: awsvolumeid(volumeId)
+            Values: awsvolumeid
         }
     ]
 };
 
 
 ec2.describeSnapshots(params, function (err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else {
-        for (var i = 0; i < data.Snapshots.length; i++) {
-            var creation = data.Snapshots[i].StartTime;
-            var createdTimestamp = new Date(creation).getTime();
-            if ((timestampNow - createdTimestamp) / 1000 / 60 / 60 / 24 > config.amazon.keepDay) {
-                removeSnapshot(data.Snapshots[i].SnapshotId);
-                var deleMsg = 'Remove backup completed';
-
-            }
+        if (err) console.log(err, err.stack); // an error occurred
+        else {
+            _.each(data.Snapshots, function (info) {
+                if ((timestampNow - new Date(info.StartTime).getTime()) / 1000 / 60 / 60 / 24 > config.amazon.keepDay) {
+                    //removeSnapshot(info.SnapshotId);
+                    var deleMsg = 'Remove backup completed';
+                }
+            });
+            createSnapshot(volumeId);
         }
-        createSnapshot(volumeId);
-    }
 
-    //STANDARD REMOVE MESSAGE
-    deleMsg = 'Remove backup has nothing to delete';
+        //STANDARD REMOVE MESSAGE
+        deleMsg = 'Remove backup has nothing to delete';
 
-    function removeSnapshot(id) {
-        var params = {
-            SnapshotId: id,
-            DryRun: false
-        };
-        ec2.deleteSnapshot(params, function (err, data) {
-            if (err) console.log(err, err.stack);// an error occurred
-            else {
-                deleMsg = 'Completed Remove Backup';
-            }
-        });
-
-    }
-
-    var k = 0;
-
-    function createSnapshot(volumeId) {
-        setTimeout(function () {
+        function removeSnapshot(id) {
             var params = {
-                VolumeId: volumeId[k],
-                Description: volumeName[k] + new Date(),
+                SnapshotId: id,
                 DryRun: false
             };
-            ec2.createSnapshot(params, function (err, data) {
-                if (err) {
-                    console.log(err, err.stack);// an error occurred
-                } else {
+            ec2.deleteSnapshot(params, function (err, data) {
+                if (err) console.log(err, err.stack);// an error occurred
+                else {
+                    deleMsg = 'Completed Remove Backup';
                 }
-                console.log(volumeName[k - 1] + ' Backed up')
             });
-            k++;
-            if (k < volumeId.length) {
-                createSnapshot(volumeId);
-            }
-        }, backupint)
+
+        }
+
+        var volNumber = 0;
+
+        function createSnapshot(volumeId) {
+            setTimeout(function () {
+                var params = {
+                    VolumeId: volumeId[volNumber],
+                    Description: volumeName[volNumber] + new Date(),
+                    DryRun: false
+                };
+                ec2.createSnapshot(params, function (err, data) {
+                    if (err) {
+                        console.log(err, err.stack);// an error occurred
+                    } else {
+                    }
+                    console.log(volumeName[volNumber - 1] + ' Backed up')
+                });
+                volNumber++;
+                if (volNumber < volumeId.length) {
+                    createSnapshot(volumeId);
+                }
+            }, backupint)
+        }
+
+
+        // send confirmation e-mail
+        var compMsg = 'Backup successful for ' + awsvolumeName;
+        SendToEmail(compMsg, deleMsg);
+
+        function SendToEmail(mcomplete, mdelete) {
+            //Email
+            mailer.Send(
+                //From
+                '"backup@ec2service.com" ' + emailAddress,
+                //To
+                sendEmailAddress,
+                //Subject
+                'Backup Log',
+                //Body Text
+                mcomplete + "\n\xA0" + mdelete);
+        }
+
     }
-
-    // send confirmation e-mail
-    var compMsg = 'Backup successful for ' + awsvolumeName(volumeId);
-    SendToEmail(compMsg, deleMsg);
-
-    function SendToEmail(mcomplete, mdelete) {
-        //Email
-        mailer.Send(
-            //From
-            '"backup@ec2service.com" ' + emailAddress,
-            //To
-            sendEmailAddress,
-            //Subject
-            'Backup Log',
-            //Body Text
-            mcomplete + "\n\xA0" + mdelete);
-    }
-
-});
+);
